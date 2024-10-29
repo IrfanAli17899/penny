28299981a433fb48e1cf1821a1309130eb2a2bdc
Irfan Ali <irfanali.17899@gmail.com>
2024-10-29 18:35:17 +0500

    fix: env var name fixed

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -7,15 +7,13 @@ import { ConfigService } from "../config/config.service";
 @Global()
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb+srv://irfanali17899:hj1ZNkEZj3pTC0RS@cluster0.t62ln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
-        // ConfigModule,
-        // MongooseModule.forRootAsync({
-        //     // imports: [ConfigModule],
-        //     useFactory: async (configService: ConfigService) => ({
-        //         uri: configService.MONGOOSE_URI, 
-        //     }),
-        //     inject: [ConfigService],
-        // })
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.MONGOOSE_URI,
+            }),
+            inject: [ConfigService],
+        })
     ],
 })
 export class DatabaseModule { }
